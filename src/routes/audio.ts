6301a79d0a24d053a0e8447ce42e60c954011421
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-18 02:57:21 -0300

    feat: convert video in audio

--- src/routes/audio.ts
+++ src/routes/audio.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
+import ytdl from "ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+import ffmpegStatic from "ffmpeg-static";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 type MyRequest = FastifyRequest<{
@@ -13,8 +17,73 @@ const audio = async (app: FastifyInstance) => {
       return reply.code(400).send("Missing videoId query parameter");
     }
 
+    const downloadVideo = () =>
+      new Promise((resolve, reject) => {
+        const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+
+        ytdl(videoURL, {
+          quality: "lowestaudio",
+          filter: "audioonly",
+        })
+          .pipe(fs.createWriteStream("video.mp4"))
+          .on("finish", () => {
+            console.log("Download finished");
+            resolve("Download finished");
+          })
+          .on("error", (error: Error) => {
+            console.error("Download failed", error);
+            reject(error);
+          });
+      });
+
+    const convertToMp3 = () =>
+      new Promise((resolve, reject) => {
+        if (ffmpegStatic === null) {
+          throw new Error("ffmpegStatic is null");
+        }
+
+        ffmpeg.setFfmpegPath(ffmpegStatic);
+
+        ffmpeg()
+          .input("video.mp4")
+          .audioCodec("libmp3lame")
+          .outputOptions(
+            "-vn",
+            "-ar",
+            "44100",
+            "-ac",
+            "2",
+            "-ab",
+            "20k",
+            "-f",
+            "mp3"
+          )
+          .saveToFile("audio.mp3")
+          .on("end", () => {
+            console.log("Conversion finished");
+            fs.unlink("video.mp4", (error) => {
+              if (error) {
+                console.error("Error deleting video file", error);
+                reject(error);
+              } else {
+                console.log("Video file deleted");
+                resolve("Conversion finished");
+              }
+            });
+          })
+          .on("error", (error: Error) => {
+            console.error("Conversion failed", error);
+            reject(error);
+          });
+      });
+
     try {
-      return reply.send(`Audio for video ${videoId}`);
+      await downloadVideo();
+      await convertToMp3();
+      const mp3Data = fs.readFileSync("audio.mp3");
+      reply.header("Content-Disposition", "attachment; filename=audio.mp3");
+      reply.header("Content-Type", "audio/mpeg");
+      return reply.send(mp3Data);
     } catch (error) {
       console.error(error);
       return reply.send(error);
