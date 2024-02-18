e1999448d0a58e120617d8aff104500f5af18ead
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-18 05:04:55 -0300

    feat: add openai

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { audio } from "routes";
 
 const app = fastify();
 
+app.register(cors);
 app.register(audio);
 
 app.listen({ port: 3333 }).then(() => {
