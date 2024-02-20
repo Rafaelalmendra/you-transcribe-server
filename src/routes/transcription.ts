6fb96e11ddfe6e02e9d55c4dc7d2ab3b6c1ff47f
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-20 02:05:25 -0300

    feat: add language param

--- src/routes/transcription.ts
+++ src/routes/transcription.ts
@@ -2,30 +2,31 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { downloadVideo, convertVideoToMp3 } from "utils";
 import { client } from "lib";
 import fs from "fs";
-import { TranscribeParams } from "assemblyai";
+import { TranscribeParams, TranscriptLanguageCode } from "assemblyai";
 
 type MyRequest = FastifyRequest<{
   Querystring: {
     videoId: string;
+    lang?: string;
   };
 }>;
 
 const transcription = async (app: FastifyInstance) => {
   app.get("/transcription", async (request: MyRequest, reply: FastifyReply) => {
     const videoId = request.query.videoId;
+    const lang = request.query.lang as string;
+
     if (!videoId) return reply.code(400).send("Missing videoId parameter");
 
     try {
       await downloadVideo(videoId);
       await convertVideoToMp3();
 
+      const language = lang || "pt";
+
       const params: TranscribeParams = {
         audio: "audio.mp3",
-        language_detection: true,
-        speaker_labels: true,
-        summarization: true,
-        summary_model: "informative",
-        summary_type: "bullets",
+        language_code: language as TranscriptLanguageCode,
       };
 
       const transcription = await client.transcripts.transcribe(params);
