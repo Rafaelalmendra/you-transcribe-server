d4dc5b8024e174067948c8942907896222817a6e
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-20 00:21:32 -0300

    feat: config port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import dotenv from "dotenv";
 import { transcription } from "routes";
+import { env } from "env";
 
 dotenv.config();
 const app = fastify();
@@ -9,6 +10,10 @@ const app = fastify();
 app.register(cors);
 app.register(transcription);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("Server is running");
-});
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then((address) => {
+    console.log(`Server is now listening on -> ${address}`);
+  });
