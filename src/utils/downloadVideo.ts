f7a12b61c42ed7a432a96bf8f9d00424af1f6c88
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-23 23:40:31 -0300

    refactor: improve download code

--- src/utils/downloadVideo.ts
+++ src/utils/downloadVideo.ts
@@ -5,22 +5,27 @@ import ytdl from "ytdl-core";
 process.env.YTDL_NO_UPDATE = "true";
 
 const downloadVideo = (videoId: string) =>
-  new Promise((resolve, reject) => {
+  new Promise(async (resolve, reject) => {
     const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
 
-    ytdl(videoURL, {
-      quality: "lowestaudio",
-      filter: "audioonly",
-    })
-      .pipe(fs.createWriteStream("video.mp4"))
-      .on("finish", () => {
-        console.log("Download finished");
-        resolve("Download finished");
-      })
-      .on("error", (error: Error) => {
-        console.error("Download failed: ", error);
-        reject(error);
+    try {
+      const response = await ytdl.getInfo(videoURL);
+      const format = ytdl.chooseFormat(response.formats, {
+        quality: "lowest",
       });
+
+      const outputFilePath = `video.mp4`;
+      const outputStream = fs.createWriteStream(outputFilePath);
+
+      ytdl(videoURL, { format }).pipe(outputStream);
+      outputStream.on("finish", () => {
+        console.log("Download finished");
+        resolve(outputFilePath);
+      });
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
   });
 
 export { downloadVideo };
